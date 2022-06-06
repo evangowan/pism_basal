c5d8ba9f955e7a3089ed5179b473ace19225daed
evangowan <evangowan@gmail.com>
2022-06-06 22:20:32 +0900

    list crazy

--- src/hydrology/hydrologyEvan.cc
+++ src/hydrology/hydrologyEvan.cc
@@ -523,16 +523,16 @@ void hydrologyEvan::basal_potential(IceModelVec2S &result) {
   double u, v;
 
   {
-    IceModelVec::AccessList list;
-
 
     const IceModelVec2S &surface_elevation = *m_grid->variables().get_2d_scalar("surface_altitude");
 
     const IceModelVec2S &bed_elevation = *m_grid->variables().get_2d_scalar("bedrock_altitude");
 
-    list.add(m_basal_potential_temp);
-    list.add(&bed_elevation);
-    list.add(&surface_elevation);
+    IceModelVec::AccessList list{m_basal_potential_temp, &bed_elevation, &surface_elevation};
+
+//    list.add(m_basal_potential_temp);
+//    list.add(&bed_elevation);
+//    list.add(&surface_elevation);
 
 
     ParallelSection loop(m_grid->com);

a5fa73c3631f87c2c6bbc90e6f2c0f96407fcd21
evangowan <evangowan@gmail.com>
2022-06-15 19:41:21 +0900

    bracket problem

--- src/hydrology/hydrologyEvan.cc
+++ src/hydrology/hydrologyEvan.cc
@@ -547,86 +547,84 @@ void hydrologyEvan::basal_potential(IceModelVec2S &result) {
       loop.failed();
     }
     loop.check();
-  }
 
-
-  m_basal_potential_temp.update_ghosts();
+    m_basal_potential_temp.update_ghosts();
 
 
     // Set grid_width to 5 to match the gradient calculation
-  int grid_width = 5; 
+    int grid_width = 5; 
 
-  // Gausian smoothing array, taken from https://homepages.inf.ed.ac.uk/rbf/HIPR2/gsmooth.htm
-  double point_store5[5][5];
+    // Gausian smoothing array, taken from https://homepages.inf.ed.ac.uk/rbf/HIPR2/gsmooth.htm
+    double point_store5[5][5];
 
-  point_store5[0][0] = 1.0;
-  point_store5[1][0] = 4.0;
-  point_store5[2][0] = 7.0;
-  point_store5[3][0] = 4.0;
-  point_store5[4][0] = 1.0;
+    point_store5[0][0] = 1.0;
+    point_store5[1][0] = 4.0;
+    point_store5[2][0] = 7.0;
+    point_store5[3][0] = 4.0;
+    point_store5[4][0] = 1.0;
 
-  point_store5[0][1] = 4.0;
-  point_store5[1][1] = 16.0;
-  point_store5[2][1] = 26.0;
-  point_store5[3][1] = 16.0;
-  point_store5[4][1] = 4.0;
-
-
-  point_store5[0][2] = 7.0;
-  point_store5[1][2] = 26.0;
-  point_store5[2][2] = 41.0;
-  point_store5[3][2] = 26.0;
-  point_store5[4][2] = 7.0;
-
-  point_store5[0][3] = 4.0;
-  point_store5[1][3] = 16.0;
-  point_store5[2][3] = 26.0;
-  point_store5[3][3] = 16.0;
-  point_store5[4][3] = 4.0;
-
-  point_store5[0][4] = 1.0;
-  point_store5[1][4] = 4.0;
-  point_store5[2][4] = 7.0;
-  point_store5[3][4] = 4.0;
-  point_store5[4][4] = 1.0;
-
-  double sum_all = 273.0;
-
-  ParallelSection loop(m_grid->com);
-  try {
+    point_store5[0][1] = 4.0;
+    point_store5[1][1] = 16.0;
+    point_store5[2][1] = 26.0;
+    point_store5[3][1] = 16.0;
+    point_store5[4][1] = 4.0;
 
 
-    double potential_sum;
-    int i_check, j_check;
+    point_store5[0][2] = 7.0;
+    point_store5[1][2] = 26.0;
+    point_store5[2][2] = 41.0;
+    point_store5[3][2] = 26.0;
+    point_store5[4][2] = 7.0;
 
-    for (Points p(*m_grid); p; p.next()) {
-      const int i = p.i(), j = p.j();
+    point_store5[0][3] = 4.0;
+    point_store5[1][3] = 16.0;
+    point_store5[2][3] = 26.0;
+    point_store5[3][3] = 16.0;
+    point_store5[4][3] = 4.0;
 
-      potential_sum = 0.0;
+    point_store5[0][4] = 1.0;
+    point_store5[1][4] = 4.0;
+    point_store5[2][4] = 7.0;
+    point_store5[3][4] = 4.0;
+    point_store5[4][4] = 1.0;
 
-      for (int k=0; k < grid_width; k++) {
-        for (int l=0; l < grid_width; l++) {
+    double sum_all = 273.0;
 
-         i_check = i + k - ((grid_width-1)/2);
-         i_check = low_check(i_check);
-         i_check = high_i_check(i_check);     
+    ParallelSection loop(m_grid->com);
+    try {
 
-         j_check = j + l - ((grid_width-1)/2);
-         j_check = low_check(j_check);
-         j_check = high_j_check(j_check);
 
-         potential_sum += m_basal_potential_temp(i_check,j_check) * point_store5[k][l];
+      double potential_sum;
+      int i_check, j_check;
+
+      for (Points p(*m_grid); p; p.next()) {
+        const int i = p.i(), j = p.j();
+
+        potential_sum = 0.0;
+
+        for (int k=0; k < grid_width; k++) {
+          for (int l=0; l < grid_width; l++) {
+
+           i_check = i + k - ((grid_width-1)/2);
+           i_check = low_check(i_check);
+           i_check = high_i_check(i_check);     
+
+           j_check = j + l - ((grid_width-1)/2);
+           j_check = low_check(j_check);
+           j_check = high_j_check(j_check);
+
+           potential_sum += m_basal_potential_temp(i_check,j_check) * point_store5[k][l];
+          }
         }
+        m_basal_potential_temp(i,j) = potential_sum / sum_all;
       }
-      m_basal_potential_temp(i,j) = potential_sum / sum_all;
+    } catch (...) {
+      loop.failed();
     }
-  } catch (...) {
-    loop.failed();
+    loop.check();
+
+    result.copy_from(m_basal_potential_temp);
   }
-  loop.check();
-
-  result.copy_from(m_basal_potential_temp);
-
 
 }
 
